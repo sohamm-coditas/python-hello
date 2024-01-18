32a7f4a7c1446a0304f866dec4b6cb3a80663d83
Soham <109778086+sohamm-coditas@users.noreply.github.com>
2024-01-18 16:53:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ from pyramid.response import Response
 import os
 
 def hello_world(request):
-    name = os.environ.get('NAME')
+    name = "demo"
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
+    port = 8000
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
